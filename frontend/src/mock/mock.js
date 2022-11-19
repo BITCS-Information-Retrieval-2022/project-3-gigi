86d76e0a7a262f4fc98c66cab394a99d428ca5aa
xuzf-git <xuzhengfei-email@qq.com>
2022-11-19 11:42:00 +0800

    modify the mockjs for test

--- frontend/src/mock/mock.js
+++ frontend/src/mock/mock.js
@@ -1,9 +1,9 @@
 /*
- * @File    :  /frontend/src/mock/mock.js
+ * @File    :  /project-3-gigi/frontend/src/mock/mock.js
  * @Author  :  xuzf
  * @Contact :  xuzhengfei-email@qq.com
  * @Create  :  2022-11-12 10:54:23
- * @Update  :  2022-11-12 11:18:25
+ * @Update  :  2022-11-19 11:40:26
  * @Desc    :  None
  */
 //引入mockjs
@@ -11,27 +11,43 @@ import { mock } from 'mockjs';   //安装的mockjs，并不是创建的mock.js
 
 //使用mockjs模拟数据
 mock(RegExp('http://mock/api/search' + '.*'), 'get', function () { //当post或get请求到/api/data路由时Mock会拦截请求并返回上面的数据
-    let menu = [
+    let hitList_new = [
         {
-            title: "资料检索",
-            list: ["条件检索", "关键字检索"],
-            id: 1
+            title: "Multiplex Graph Neural Network for Extractive Text Summarization",
+            authors: ["Baoyu Jing", "Zeyu You", "Tao Yang", "Wei Fan", "Hanghang Tong"],
+            abstract: "Extractive text summarization aims at extracting the most representative sentences from a given document as its summary. To extract a good summary from a long text document, sentence embedding plays an important role. Recent studies have leveraged graph neural networks to capture the inter-sentential relationship (e.g., the discourse graph) within the documents to learn contextual sentence embedding. However, those approaches neither consider multiple types of inter-sentential relationships (e.g., semantic similarity and natural connection relationships), nor model intra-sentential relationships (e.g, semantic similarity and syntactic relationship among words). To address these problems, we propose a novel Multiplex Graph Convolutional Network (Multi-GCN) to jointly model different types of relationships among sentences and words. Based on Multi-GCN, we propose a Multiplex Graph Summarization (Multi-GraS) model for extractive text summarization. Finally, we evaluate the proposed models on the CNN/DailyMail benchmark dataset to demonstrate effectiveness of our method.",
+            doi: "10.18653/v1/2021.emnlp-main.11",
+            year: 2021,
+            month: 11,
+            type: "Conference",
+            venue: "EMNLP",
+            volume: "Proceedings of the 2021 Conference on Empirical Methods in Natural Language Processing",
+            source_url: "https://aclanthology.org/2021.emnlp-main.11/",
+            pdf_url: "https://aclanthology.org/2021.emnlp-main.11.pdf",
+            ebook_url: "https://aclanthology.org/2021.emnlp-main.11.pdf",
+            ppt_url: "https://www.slideserve.com/vera/text-summarization",
+            video_url: "https://aclanthology.org/2021.emnlp-main.11.mp4",
+            citations_num: 31,         // 被引次数
+            // reference_list: [ref1, ref2, ...]     // 参考文献列表
         },
         {
-            title: "借阅管理",
-            list: ["条件检索", "关键字检索"],
-            id: 2
-        },
-        {
-            title: "统计分析",
-            list: ["条件检索", "关键字检索"],
-            id: 3
-        },
-        {
-            title: "系统管理",
-            list: ["条件检索", "关键字检索"],
-            id: 4
-        },
+            title: "Multiplex Graph Neural Network for Extractive Text Summarization",
+            authors: ["Baoyu Jing", "Zeyu You", "Tao Yang", "Wei Fan", "Hanghang Tong"],
+            abstract: "Extractive text summarization aims at extracting the most representative sentences from a given document as its summary. To extract a good summary from a long text document, sentence embedding plays an important role. Recent studies have leveraged graph neural networks to capture the inter-sentential relationship (e.g., the discourse graph) within the documents to learn contextual sentence embedding. However, those approaches neither consider multiple types of inter-sentential relationships (e.g., semantic similarity and natural connection relationships), nor model intra-sentential relationships (e.g, semantic similarity and syntactic relationship among words). To address these problems, we propose a novel Multiplex Graph Convolutional Network (Multi-GCN) to jointly model different types of relationships among sentences and words. Based on Multi-GCN, we propose a Multiplex Graph Summarization (Multi-GraS) model for extractive text summarization. Finally, we evaluate the proposed models on the CNN/DailyMail benchmark dataset to demonstrate effectiveness of our method.",
+            doi: "10.18653/v1/2021.emnlp-main.11",
+            year: 2021,
+            month: 11,
+            type: "Conference",
+            venue: "EMNLP",
+            volume: "Proceedings of the 2021 Conference on Empirical Methods in Natural Language Processing",
+            source_url: "https://aclanthology.org/2021.emnlp-main.11/",
+            pdf_url: "https://aclanthology.org/2021.emnlp-main.11.pdf",
+            ebook_url: "https://aclanthology.org/2021.emnlp-main.11.pdf",
+            ppt_url: "https://www.slideserve.com/vera/text-summarization",
+            video_url: "https://aclanthology.org/2021.emnlp-main.11.mp4",
+            citations_num: 12,         // 被引次数
+            // reference_list: [ref1, ref2, ...]     // 参考文献列表
+        }
     ];
     console.log("sdasdasdasdasdadadasda!!!!!!!!")
     let hitList = [
@@ -48,9 +64,11 @@ mock(RegExp('http://mock/api/search' + '.*'), 'get', function () { //当post或g
     ]
 
     return {
-        totalNums: 1,
-        searchCostTime: 0.01,
-        hitList: hitList
+        page: 1,                                   // 当前第几页（从1开始）
+        searchCostTime: 0.01, // 搜索用时（单位为秒）
+        totalNums: 10,
+        pageNums: 3,
+        hitList: hitList_new
     }
 })
 
